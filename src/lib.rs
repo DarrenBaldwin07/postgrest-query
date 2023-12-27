58f9e048c76c7ada1792dbdb32c9a3559ac5016f
Darren B <68653294+Devd0@users.noreply.github.com>
2023-12-26 19:52:45 -0500

    rm cargo lib defaults

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,2 @@
 pub mod client;
 pub mod builder;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
