2fdce10abb68a3d45d2d17f6977d2edbeaec1370
Darren B <68653294+Devd0@users.noreply.github.com>
2023-12-28 08:07:01 -0500

    clean up exec

--- src/filter.rs
+++ src/filter.rs
@@ -1,10 +1,10 @@
 use reqwest::{Client, Method};
-use std::collections::HashMap;
-use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use std::{collections::HashMap, f32::consts::E};
+use serde::{Serialize, de::DeserializeOwned, Deserialize};
 use url::Url;
 
 #[derive(Debug)]
-enum FilterType {
+pub enum FilterType {
 	Eq,
 	Neq,
 	Gt,
@@ -29,6 +29,20 @@ enum FilterType {
 	Wfts,
 }
 
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PostgrestErrorResponse {
+    pub hint: String,
+    pub details: serde_json::Value,
+    pub code: String,
+    pub message: String,
+}
+
+pub enum PostgrestError {
+    PostgrestErrorResponse(PostgrestErrorResponse),
+    ReqwestError(reqwest::Error),
+}
+
 pub struct PostgrestFilter<T> where T: Serialize + DeserializeOwned {
 	pub url: Url,
 	pub headers: HashMap<String, String>,
@@ -41,9 +55,17 @@ impl<T> PostgrestFilter<T> where T: Serialize + DeserializeOwned {
 		PostgrestFilter { url, headers, method, _marker: std::marker::PhantomData }
 	}
 
-    // TODO: question this builder pattern for filtering a little - maybe we can make this better?
+    // TODO: question this builder pattern for filtering - maybe we can make this better?
     pub fn eq(&mut self, column: &str, value: &str) {
         self.url.query_pairs_mut().append_pair(column, format!("eq.{}", value).as_str());
+    }
+
+    pub fn neq(&mut self, column: &str, value: &str) {
+        self.url.query_pairs_mut().append_pair(column, format!("neq.{}", value).as_str());
+    }
+
+    pub fn gt(&mut self, column: &str, value: &str) {
+        self.url.query_pairs_mut().append_pair(column, format!("gt.{}", value).as_str());
     }
 
 	pub fn filter(values: T, filter_method: FilterType) {}
@@ -51,18 +73,28 @@ impl<T> PostgrestFilter<T> where T: Serialize + DeserializeOwned {
     // TODO: like the normal `.exec()` but with the blocking reqwest client
 	pub async fn exec_blocking(self) {}
 
-	pub async fn exec(self) -> Result<T, reqwest::Error> {
+	pub async fn exec(self) -> Result<T, PostgrestError> {
 		let client = Client::new();
 		let res = client.request(self.method, self.url).send().await;
 
         match res {
             Ok(res) => {
-                return res.json::<T>().await;
+                if res.status().is_success() {
+                    match res.json::<T>().await {
+                        Ok(res) => {
+                            return Ok(res);
+                        }
+                        Err(e) => {
+                            return Err(PostgrestError::ReqwestError(e));
+                        }
+                    }
+                }
+                let err = res.json::<PostgrestErrorResponse>().await.unwrap();
+                return Err(PostgrestError::PostgrestErrorResponse(err));
             }
             Err(e) => {
-                return Err(e);
+                return Err(PostgrestError::ReqwestError(e));
             }
         }
 	}
 }
-
