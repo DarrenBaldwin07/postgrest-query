e2535f8cb816eab14ed8f94eeba8fcfacd736912
Darren B <68653294+Devd0@users.noreply.github.com>
2023-12-28 19:20:33 -0500

    set up initial .from

--- src/client.rs
+++ src/client.rs
@@ -1,16 +1,24 @@
+use reqwest::header::HeaderMap;
 use crate::builder::PostgresQueryBuilder;
 
 pub struct PostgrestClient {
 	pub url: String,
+	pub headers: HeaderMap
 }
 
 impl PostgrestClient {
-	pub fn new(url: String) -> PostgrestClient {
-		PostgrestClient { url }
+	pub fn new(url: String, headers: HeaderMap) -> PostgrestClient {
+		PostgrestClient { url, headers }
 	}
 
-	pub fn from(&self, relation: &str) {
+	pub fn from(&self, relation: &str) -> PostgresQueryBuilder {
 		let url = format!("{}/{}", self.url, relation);
-		// This will just return a new PostgresQueryBuilder
+		PostgresQueryBuilder::new(url, self.headers.clone())
 	}
+
+	/// TODO
+	fn schema() {}
+
+	/// TODO
+	fn rpc() {}
 }
